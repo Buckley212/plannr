11ba998e1bcc014341a8b8d9b6d5ed49c9b0edee
Cody Thigpen <ckthigpen1@gmail.com>
2021-04-26 17:46:56 -0400

    added HomePage

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
+
 function HomePage(props) {
 
 
 
     return (
         <div>
-
+            <h1>Welcome to Plannr!</h1>
+            <div className='Flex'>
+                <div className="form-floating mb-3">
+                    <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                    <label for="floatingInput">Destination</label>
+                </div>
+                <div className="form-floating mb-3">
+                    <input type="date" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                    <label for="floatingInput">Departure Date/Time</label>
+                </div>
+                <div className="form-floating mb-3">
+                    <input type="date" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                    <label for="floatingInput">Arrival Date/Time</label>
+                </div>
+            </div>
         </div>
     );
 }
